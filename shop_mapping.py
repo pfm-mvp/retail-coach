c09d6f605834642b08ea633db18457cbe7d5da07
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-08-10 09:44:11 +0200

    Add files via upload

---
+++ shop_mapping.py
@@ -0,0 +1,10 @@
+SHOP_NAME_MAP = {
+    32224: "Amersfoort",
+    31977: "Amsterdam",
+    31831: "Den Bosch",
+    32319: "Leiden",
+    30058: "Nijmegen",
+    32320: "Rotterdam",
+    32204: "Venlo"
+}
+
